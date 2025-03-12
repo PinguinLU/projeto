7fb84a7ebd212f9c6ba443e36683252a3bdb8fa4
Lucas Figueiredo <lucasfigueiredoqueirozz@hotmail.com>
2025-03-11 21:37:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+    const html = document.documentElement; // Acessa o elemento <html>
+    const img = document.querySelector("#profile img"); // Acessa a imagem do perfil
+
+    // Alterna a classe 'light' no <html>
+    html.classList.toggle("light");
+
+    // Verifica se a classe 'light' foi adicionada
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/Lucas-light.png'); // Altera a imagem para a versão clara
+    } else {
+        img.setAttribute('src', './assets/Lucas.png'); // Altera a imagem para a versão escura
+    }
+}
